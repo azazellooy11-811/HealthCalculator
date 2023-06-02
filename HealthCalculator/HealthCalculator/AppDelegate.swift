afe2e021d8cf1a7528e14f658248b7ebc6a9acdd
azazellooy11-811 <khismatova_05@mail.ru>
2023-06-02 14:58:51 +0300

    Project created

---
+++ HealthCalculator/HealthCalculator/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  HealthCalculator
+//
+//  Created by Азалия Халилова on 02.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window {
+            let navigationController = UINavigationController()
+            let viewController = UIViewController()
+            navigationController.viewControllers = [viewController]
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
+
---
+++ HealthCalculator/HealthCalculator/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  HealthCalculator
+//
+//  Created by Азалия Халилова on 02.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
